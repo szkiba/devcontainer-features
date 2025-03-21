484b3adeeb89239850c488bf16001cc59bf19a6d
Iván SZKIBA <szkiba@users.noreply.github.com>
2025-03-21 07:21:31 +0000

    test: use previous version to test version parameter handling

--- test/gosec/gosec-with-version.sh
+++ test/gosec/gosec-with-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This test file will be executed against one of the scenarios devcontainer.json test that
-# includes the 'gosec' feature with "version": "2.22.2" option.
+# includes the 'gosec' feature with "version": "2.22.1" option.
 
 set -e
 
@@ -9,7 +9,7 @@ set -e
 source dev-container-features-test-lib
 
 # Feature-specific tests
-check "execute command" bash -c "gosec --version | grep 'Version: 2.22.2'"
+check "execute command" bash -c "gosec --version | grep 'Version: 2.22.1'"
 
 # Report results
 reportResults
--- test/govulncheck/govulncheck-with-version.sh
+++ test/govulncheck/govulncheck-with-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This test file will be executed against one of the scenarios devcontainer.json test that
-# includes the 'govulncheck' feature with "version": "1.1.4" option.
+# includes the 'govulncheck' feature with "version": "1.1.3" option.
 
 set -e
 
@@ -9,7 +9,7 @@ set -e
 source dev-container-features-test-lib
 
 # Feature-specific tests
-check "execute command" bash -c "govulncheck -version | grep 'Scanner: govulncheck@v1.1.4'"
+check "execute command" bash -c "govulncheck -version | grep 'Scanner: govulncheck@v1.1.3'"
 
 # Report results
 reportResults
